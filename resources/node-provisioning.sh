c9b7bf8c016c26b5eabd1e0f250f42a12966d4b5
Bram de Kruijff <bdekruijff@gmail.com>
2014-07-25 09:17:35 +0200

    Provide a cmdline option to run with interactive shell
    Allow the of -i to run in interactive mode for debugging purposes

--- resources/node-provisioning.sh
+++ resources/node-provisioning.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-
+GOSH_NONINTERACTIVE=true
 PWD=`pwd`
+
+for var in "$@"; do
+  if [ "$var" == "-i" ]; then
+    GOSH_NONINTERACTIVE=false
+  fi
+done
+
+JAVA_PROPS="-Dace.gogo.script=default-mapping.gosh"
+
+if $GOSH_NONINTERACTIVE; then
+  JAVA_PROPS="$JAVA_PROPS -Dgosh.args=--nointeractive"
+fi
+
 cd $(dirname $0)
-
-java -Dace.gogo.script=default-mapping.gosh -Dgosh.args=--nointeractive -jar server-allinone.jar
-
+java $JAVA_PROPS -jar server-allinone.jar
 cd $PWD
